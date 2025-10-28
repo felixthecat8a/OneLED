5d21cd28cb22c0db18d4e8c244d4feefab2113ff
Felix Ochoa <115331149+felixthecat8a@users.noreply.github.com>
2025-10-27 22:34:49 -0500

    Refactor PulseWidthModulatedLED for ESP32 support

--- src/util/PulseWidthModulated.cpp
+++ src/util/PulseWidthModulated.cpp
@@ -3,16 +3,31 @@
 PulseWidthModulatedLED::PulseWidthModulatedLED(uint8_t pin, bool reverse)
   : _pin(pin), _brightness(0), _reverse(reverse) {}
 
-bool PulseWidthModulatedLED::begin() {
-  if (_pin < 0 ) { return false; }
-  pinMode(_pin, OUTPUT);
-  analogWrite(_pin, _reverse ? 255 : 0);
-  return true;
+void PulseWidthModulatedLED::begin() {
+  #ifdef ESP32
+    #if ESP_ARDUINO_VERSION_MAJOR >= 3
+      ledcAttach(_pin, 5000, 8);  // 5 kHz frequency, 8-bit resolution
+    #else
+      ledcSetup(_pin, 5000, 8);
+      ledcAttachPin(_pin, _pin);
+    #endif
+  #else
+    pinMode(_pin, OUTPUT);
+  #endif
+  _write(_reverse ? 255 : 0);
+}
+
+void PulseWidthModulatedLED::_write(uint8_t value) {
+  #ifdef ESP32
+    ledcWrite(_pin, value);
+  #else
+    analogWrite(_pin, value);
+  #endif
 }
 
 void PulseWidthModulatedLED::setBrightness(uint8_t brightness) {
   _brightness = brightness;
-  analogWrite(_pin, _reverse ? (255 - _brightness) : _brightness);
+  _write(_reverse ? (255 - _brightness) : _brightness);
 }
 
 uint8_t PulseWidthModulatedLED::getBrightness() const {
@@ -21,7 +36,7 @@ uint8_t PulseWidthModulatedLED::getBrightness() const {
 
 void PulseWidthModulatedLED::setReverse(bool reverse) {
   _reverse = reverse;
-  analogWrite(_pin, _reverse ? (255 - _brightness) : _brightness);
+  _write(_reverse ? (255 - _brightness) : _brightness);
 }
 
 bool PulseWidthModulatedLED::isReverse() const {
@@ -30,9 +45,10 @@ bool PulseWidthModulatedLED::isReverse() const {
 
 void PulseWidthModulatedLED::off() {
   _brightness = 0;
-  analogWrite(_pin, _reverse ? 255 : 0);
+  _write(_reverse ? 255 : 0);
 }
 
 bool PulseWidthModulatedLED::isOn() const {
   return _reverse ? _brightness < 255 : _brightness > 0;
 }
+
