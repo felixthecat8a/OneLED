e413cc05a6ca29fc33616cbff16684a6c14c8d39
Felix Ochoa <115331149+felixthecat8a@users.noreply.github.com>
2025-10-27 22:31:45 -0500

    Add getter methods and rename color constants

--- src/util/RedGreenBlueLED.h
+++ src/util/RedGreenBlueLED.h
@@ -10,6 +10,9 @@ class RedGreenBlueLED {
     void setRGB(const uint8_t rgb[3]);
     void setRGB(uint8_t red, uint8_t green, uint8_t blue);
     const uint8_t* getRGB() const;
+    const uint8_t getRed() const;
+    const uint8_t getGreen() const;
+    const uint8_t getBlue() const;
     void setHex(uint32_t hex);
     const uint32_t getHex() const;
     String getHexString() const;
@@ -39,8 +42,8 @@ class RedGreenBlueLED {
     int _redPin, _greenPin, _bluePin;
     uint8_t _brightness = 255;
     uint8_t _RGB[3];
-    uint8_t setColor(uint8_t color);
-    inline void showRGB(uint8_t red, uint8_t green, uint8_t blue);
+    uint8_t _setColor(uint8_t color);
+    inline void _showRGB(uint8_t red, uint8_t green, uint8_t blue);
     static const uint8_t _gammaTable[256];
 };
 
@@ -48,18 +51,19 @@ class RedGreenBlue {
   public:
     static const uint8_t BLACK[3];
     static const uint8_t WHITE[3];
-    static const uint8_t HOT_PINK[3];
+    static const uint8_t HOTPINK[3];
     static const uint8_t RED[3];
     static const uint8_t ORANGE[3];
     static const uint8_t YELLOW[3];
-    static const uint8_t LIME_GREEN[3];
+    static const uint8_t LIMEGREEN[3];
     static const uint8_t GREEN[3];
-    static const uint8_t SPRING_GREEN[3];
+    static const uint8_t SPRING[3];
     static const uint8_t CYAN[3];
-    static const uint8_t SKY_BLUE[3];
+    static const uint8_t SKYBLUE[3];
     static const uint8_t BLUE[3];
     static const uint8_t VIOLET[3];
     static const uint8_t MAGENTA[3];
 };
 
 #endif
+
