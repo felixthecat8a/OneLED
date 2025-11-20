18a1a81c4596deafae402c8f833728b12989d876
Felix Ochoa <115331149+felixthecat8a@users.noreply.github.com>
2025-11-19 20:47:43 -0600

    Enhance OneLED class with PWM support
    Added support for PWM functionality and brightness control.

--- src/OneLED.cpp
+++ src/OneLED.cpp
@@ -1,49 +1,109 @@
 #include "OneLED.h"
 
-#ifndef LED_BUILTIN
-  #define LED_BUILTIN 13
+#ifndef ESP32_PWM_FREQ
+  #define ESP32_PWM_FREQ 5000 // 5 kHz frequency
 #endif
 
-OneLED::OneLED(uint8_t pin = LED_BUILTIN, bool reverse)
-  : _pin(pin), _state(false), _reverse(reverse) {}
+#ifndef ESP32_PWM_RES
+  #define ESP32_PWM_RES 8 // 8-bit resolution
+#endif
+
+OneLED::OneLED(uint8_t pin, bool isPWM, bool reverse)
+  : _pin(pin), _state(false), _isPWM(isPWM), _reverse(reverse), _brightness(0) {}
 
 bool OneLED::begin() {
-  if (_pin < 0 ) { return false; }
-  pinMode(_pin, OUTPUT);
-  digitalWrite(_pin, _reverse ? HIGH : LOW);
+  if (_pin < 0) { return false; }
+
+  #ifdef ESP32
+    if (_isPWM) {
+      #if ESP_ARDUINO_VERSION_MAJOR >= 3
+        ledcAttach(_pin, ESP32_PWM_FREQ, ESP32_PWM_RES);
+      #else
+        ledcSetup(_pin, ESP32_PWM_FREQ, ESP32_PWM_RES);
+        ledcAttachPin(_pin, _pin);
+      #endif
+    } else {
+      pinMode(_pin, OUTPUT);
+    }
+  #else
+    pinMode(_pin, OUTPUT);
+  #endif
+
+  writeRaw(_reverse ? 255 : 0);
   return true;
 }
 
 void OneLED::on() {
-  digitalWrite(_pin, _reverse ? LOW : HIGH);
+  if (_isPWM) {
+    setBrightness(255);
+  } else {
+    digitalWrite(_pin, _reverse ? LOW : HIGH);
+  }
   _state = true;
 }
 
 void OneLED::off() {
-  digitalWrite(_pin, _reverse ? HIGH : LOW);
+  if (_isPWM) {
+    setBrightness(0);
+  } else {
+    digitalWrite(_pin, _reverse ? HIGH : LOW);
+  }
   _state = false;
 }
 
 void OneLED::toggle() {
-  _state = !_state;
-  digitalWrite(_pin, (_state ^ _reverse) ? HIGH : LOW);
+  if (_isPWM) {
+    _brightness = _state ? 0 : 255;
+    setBrightness(_brightness);
+  } else {
+    _state = !_state;
+    digitalWrite(_pin, (_state ^ _reverse) ? HIGH : LOW);
+  }
 }
 
-bool OneLED::isOn() {
+bool OneLED::isOn() const {
+  if (_isPWM) {
+    return _reverse ? _brightness < 255 : _brightness > 0;
+  }
   return _state;
-}
-
-bool OneLED::isReverse() {
-  return _reverse;
 }
 
 void OneLED::setPin(uint8_t pin) {
   _pin = pin;
-  pinMode(_pin, OUTPUT);
-  digitalWrite(_pin, (_state ^ _reverse) ? HIGH : LOW);
+  begin();
 }
 
 void OneLED::setReverse(bool reverse) {
   _reverse = reverse;
-  digitalWrite(_pin, (_state ^ _reverse) ? HIGH : LOW);
+  if (_isPWM) {
+    setBrightness(_brightness);
+  } else {
+    digitalWrite(_pin, (_state ^ _reverse) ? HIGH : LOW);
+  }
+}
+
+bool OneLED::isReverse() const { return _reverse; }
+bool OneLED::isPWM() const { return _isPWM; }
+
+void OneLED::setBrightness(uint8_t brightness) {
+  if (!_isPWM) return;
+  _brightness = brightness;
+  writeRaw(_reverse ? (255 - _brightness) : _brightness);
+  _state = (_brightness > 0);
+}
+
+uint8_t OneLED::getBrightness() const { return _brightness; }
+
+void OneLED::writeRaw(uint8_t value) {
+  #ifdef ESP32
+    if (_isPWM)
+      ledcWrite(_pin, value);
+    else
+      digitalWrite(_pin, value > 127 ? HIGH : LOW);
+  #else
+    if (_isPWM)
+      analogWrite(_pin, value);
+    else
+      digitalWrite(_pin, value > 127 ? HIGH : LOW);
+  #endif
 }
