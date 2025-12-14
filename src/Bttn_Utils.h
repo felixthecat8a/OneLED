7d94cbe7f43c35ba3599c61788d92f417a587244
Felix Ochoa <fo59paz@yahoo.com>
2025-12-14 16:59:26 -0600

    modified:   src/Bttn_Utils.cpp

--- src/Bttn_Utils.h
+++ src/Bttn_Utils.h
@@ -5,39 +5,159 @@
 
 class Bttn_Utils {
 public:
-  Bttn_Utils(uint8_t pin, bool activeLow = true, uint16_t debounceTime = 50);
+  Bttn_Utils(uint8_t pin, bool activeLow = true, uint16_t debounceTime = 50)
+  : _pin(pin),
+    _activeLow(activeLow),
+    _debounceTime(debounceTime),
+    _holdTime(1000),
+    _doubleClickTime(300),
+    _state(State::Idle),
+    _clickEvent(false),
+    _releasedEvent(false),
+    _heldEvent(false),
+    _doubleClickEvent(false),
+    _lastDebounceTime(0),
+    _pressedTime(0),
+    _lastReleaseTime(0),
+    _lastReading(!activeLow) {}
 
-  bool begin();
-  void update();
-  bool isPressed() const;
-  bool wasPressed();
-  bool wasReleased();
-  bool wasHeld();
-  bool wasDoubleClicked();
-  void setHoldTime(uint16_t ms);
-  void setDoubleClickTime(uint16_t ms);
-  bool isWaitingForDoubleClick() const;
+  void begin() {
+    pinMode(_pin, _activeLow ? INPUT_PULLUP : INPUT);
+  }
+
+  void update() {
+    bool reading = digitalRead(_pin);
+    if (_activeLow) reading = !reading;
+
+    uint32_t now = millis();
+
+    switch (_state) {
+      case State::Idle:
+        if (reading != _lastReading) {
+          _lastDebounceTime = now;
+          _state = State::Debouncing;
+        }
+        break;
+
+      case State::Debouncing:
+        if ((now - _lastDebounceTime) >= _debounceTime) {
+          if (reading) {
+            _pressedTime = now;
+            _state = State::Pressed;
+          } else {
+            _state = State::Idle;
+          }
+        }
+        break;
+
+      case State::Pressed:
+        if (!reading) {
+          _releasedEvent = true;
+          _lastReleaseTime = now;
+          _state = State::WaitingDouble;
+        }
+        else if ((now - _pressedTime) >= _holdTime) {
+          _heldEvent = true;
+          _state = State::Held;
+        }
+        break;
+
+      case State::Held:
+        if (!reading) {
+          _releasedEvent = true;
+          _state = State::Idle;
+        }
+        break;
+
+      case State::WaitingDouble:
+        if (reading && (now - _lastReleaseTime <= _doubleClickTime)) {
+          _doubleClickEvent = true;
+          _state = State::DoubleClick;
+        }
+
+        else if (now - _lastReleaseTime > _doubleClickTime) {
+          _clickEvent = true;
+          _state = State::Idle;
+        }
+        break;
+
+      case State::DoubleClick:
+        if (!reading) {
+          _state = State::Idle;
+        }
+        break;
+    }
+
+    _lastReading = reading;
+  }
+
+  bool isDown() const {
+    return _lastReading;
+  }
+
+  bool wasClicked() {
+    if (_clickEvent) {
+      _clickEvent = false;
+      return true;
+    }
+    return false;
+  }
+
+  bool wasReleased() {
+    if (_releasedEvent) {
+      _releasedEvent = false;
+      return true;
+    }
+    return false;
+  }
+
+  bool wasHeld() {
+    if (_heldEvent) {
+      _heldEvent = false;
+      return true;
+    }
+    return false;
+  }
+
+  bool wasDoubleClicked() {
+    if (_doubleClickEvent) {
+      _doubleClickEvent = false;
+      return true;
+    }
+    return false;
+  }
+
+  void setHoldTime(uint16_t ms) {
+    _holdTime = ms;
+  }
+
+  void setDoubleClickTime(uint16_t ms) {
+    _doubleClickTime = ms;
+  }
+
+  bool isWaitingForDoubleClick() const {
+    return _state == State::WaitingDouble;
+  }
 
 private:
+  enum class State : uint8_t {
+    Idle,
+    Debouncing,
+    Pressed,
+    Held,
+    WaitingDouble,
+    DoubleClick
+  };
+
   uint8_t _pin;
   bool _activeLow;
   uint16_t _debounceTime;
   uint16_t _holdTime;
   uint16_t _doubleClickTime;
 
-  enum ButtonState {
-    BUTTON_IDLE,
-    BUTTON_DEBOUNCING,
-    BUTTON_PRESSED,
-    BUTTON_HOLD,
-    BUTTON_WAITING_DOUBLE,
-    BUTTON_DOUBLE_CLICK
-  };
+  State _state;
 
-
-  ButtonState _state;
-
-  bool _pressedEvent;
+  bool _clickEvent;
   bool _releasedEvent;
   bool _heldEvent;
   bool _doubleClickEvent;
